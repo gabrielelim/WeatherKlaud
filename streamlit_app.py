225a9470dfc89ebf2d66c578b60f0ff777b74083
Gabriel Ekai <106986585+gabrielelim@users.noreply.github.com>
2026-06-29 19:10:57 +0300

    Update app title and introductory text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("WEATHER KLAD")
+st. write(
+    "Turning complex weather information into a user-friendly format"
 )
